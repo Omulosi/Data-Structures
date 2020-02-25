737c3d714fa87d8783e8128537d5e828ae1f9ae8
Omulosi <mulongojohnpaul@gmail.com>
2020-02-25 16:18:55 +0300

    fix queue

--- queue_and_stack/dll_queue.py
+++ queue_and_stack/dll_queue.py
@@ -10,10 +10,16 @@ class Queue:
         self.storage = DoublyLinkedList()
 
     def enqueue(self, value):
-        self.add_to_head(value)
+        self.size += 1
+        self.storage.add_to_head(value)
 
     def dequeue(self):
-        self.remove_from_tail()
+        if self.size > 0:
+            self.size -= 1
+            return self.storage.remove_from_tail()
+        else:
+            return None
+
 
     def len(self):
-        return len(self.storage)
+        return self.size
